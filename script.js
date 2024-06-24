ca17f8bbb2c10a3aadd1a074f02f8fb53f09424c
Forellee <104067792+Forellee@users.noreply.github.com>
2024-06-25 01:16:04 +0300

    Add files via upload
    nikita present

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ document.getElementById('magicButton').addEventListener('click', function() {
     const box = document.getElementById('animatedBox');
     const image = document.getElementById('flyingImage');
     const music = document.getElementById('backgroundMusic');
+    const presentationText = document.getElementById('presentationText');
 
     box.style.animation = 'boxAnimation 2s infinite';
     image.classList.remove('hidden');
     music.play();
+
+    image.addEventListener('animationend', function() {
+        presentationText.classList.remove('hidden');
+    });
 });
